98b62d8d62b309e3d5895b0d92a6fd6703cc4bb8
azharcool <azharuddinmullah219@gmail.com>
2023-05-31 20:48:46 +0530

    port 5001

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 swaggerDocument = require("./swagger.json");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
